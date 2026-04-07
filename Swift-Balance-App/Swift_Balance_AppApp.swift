9fafb03b3ade67475730792bb38c697e2beb6418
sethum-VS <smwopendev@gmail.com>
2026-04-07 22:35:55 +0530

    Initial Commit

---
+++ Swift-Balance-App/Swift_Balance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Balance_AppApp.swift
+//  Swift-Balance-App
+//
+//  Created by Sethum Methsanda on 2026-04-07.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Balance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
